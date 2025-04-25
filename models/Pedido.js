3cb12baf67a037783ba45690677d7218e2a4c4c4
angelcesur22 <149780300+angelcesur22@users.noreply.github.com>
2025-04-25 12:55:53 +0200

    Add files via upload

--- models/Pedido.js
+++ models/Pedido.js
@@ -5,14 +5,19 @@ const PedidoSchema = new mongoose.Schema({
   usuario: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
   productos: [
     {
-      producto: { type: mongoose.Schema.Types.ObjectId, ref: 'Producto' },
+      producto: {
+        _id: mongoose.Schema.Types.ObjectId,
+        nombre: String,
+        marca: String
+      },
       cantidad: Number,
+      talla: String,
       precio: Number
     }
   ],
   total: Number,
-  estado: { type: String, default: "pendiente" },
-  fecha: { type: Date, default: Date.now }
+  estado: { type: String, default: "Pendiente" },
+  fecha: { type: String } // fecha ya formateada como string legible
 });
 
 module.exports = mongoose.model('Pedido', PedidoSchema);
